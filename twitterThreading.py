5613071ff481f3aecc89ea54ec45b77812d498c0
root <root@DESKTOP-OAITEL0>
2023-11-11 08:28:38 +0400

    testing succesfull with threads for all social media

--- twitterThreading.py
+++ twitterThreading.py
@@ -1,6 +1,7 @@
 import requests
 import threading
 from requests.exceptions import RequestException
+import time
 
 def make_request(payload, flag):
     try:
@@ -10,11 +11,23 @@ def make_request(payload, flag):
             auth=('hadi14250', '05590560352Hk200018'), #Your credentials go here
             json=payload,
         )
-        if (flag == 'p'):
-            with open('webScraperProfile.html', 'wb') as f:
+        if (flag == "twitterProfile"):
+            with open('twitterProfile.html', 'wb') as f:
                 f.write(response.content)
-        else:
-            with open('webScraperTweet.html', 'wb') as f:
+        elif (flag == "tweet"):
+            with open('tweet.html', 'wb') as f:
+                f.write(response.content)
+        elif (flag == "instagramProfile"):
+            with open('instagramProfile.html', 'wb') as f:
+                f.write(response.content)
+        elif (flag == "instagramPost"):
+            with open('instagramPost.html', 'wb') as f:
+                f.write(response.content)
+        elif (flag == "tiktokProfile"):
+            with open('tiktokProfile.html', 'wb') as f:
+                f.write(response.content)
+        elif (flag == "tiktokPost"):
+            with open('tiktokPost.html', 'wb') as f:
                 f.write(response.content)
     except requests.ConnectionError as e:
         print(f"An error occurred: {e}")
@@ -33,7 +46,7 @@ twitterHandle = "ordinalHO"
 twitterSession = "?t=jkhlbh&s=04"
 url = "https://x.com/{}{}".format(twitterHandle, twitterSession)
 
-profilePayload = {
+twitterProfilePayload = {
     "source": "universal",
     "url": "https://x.com/{}{}".format(twitterHandle, twitterSession),
     "render": "html",
@@ -45,13 +58,56 @@ tweetPayload = {
     "render": "html",
 }
 
-thread1 = threading.Thread(target=make_request, args=(profilePayload, 'p'))
-thread2 = threading.Thread(target=make_request, args=(tweetPayload, 't'))
+instagramProfilePayload = {
+    "source": "universal",
+    "url": "https://www.instagram.com/pubity/",
+    "render": "html",
+}
+
+instagramPostPayload = {
+    "source": "universal",
+    "url": "https://www.instagram.com/p/CzTWqUWLLvJ/",
+    "render": "html",
+}
+
+tiktokProfilePayload = {
+    "source": "universal",
+    "url": "https://www.tiktok.com/@thezachchoi",
+    "render": "html",
+}
+
+tiktokPostPayload = {
+    "source": "universal",
+    "url": "https://www.tiktok.com/@thezachchoi/video/7298038955348397354",
+    "render": "html",
+}
+
+thread1 = threading.Thread(target=make_request, args=(twitterProfilePayload, "twitterProfile"))
+# time.sleep(1)
+thread2 = threading.Thread(target=make_request, args=(tweetPayload, "tweet"))
+# time.sleep(1)
+thread3 = threading.Thread(target=make_request, args=(instagramProfilePayload, "instagramProfile"))
+# time.sleep(1)
+thread4 = threading.Thread(target=make_request, args=(instagramPostPayload, "instagramPost"))
+# time.sleep(1)
+thread5 = threading.Thread(target=make_request, args=(tiktokProfilePayload, "tiktokProfile"))
+# time.sleep(1)
+thread6 = threading.Thread(target=make_request, args=(tiktokPostPayload, "tiktokPost"))
 
 # Start the threads
 thread1.start()
 thread2.start()
+thread3.start()
+thread4.start()
+thread5.start()
+thread6.start()
+
 
 # Wait for both threads to finish
 thread1.join()
 thread2.join()
+thread3.join()
+thread4.join()
+thread5.join()
+thread6.join()
+
